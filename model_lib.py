fc7e8caab6c515a464da86d1c416e4ab3c26d81d
Tom Rutten <t.rutten15@gmail.com>
2016-12-15 20:48:55 -0500

    something is broken!!!!!!!!!!!!

--- model_lib.py
+++ model_lib.py
@@ -1,66 +1,153 @@
-n_nodes = 12
+import numpy as np
+import pdb 
 
-#calcium pairs
-calcium_pairs = [(0,1),(1,2),(2,3),(3,4),(3,5),(5,6)]
+class Neuron:
+	def __init__(self):
+		self.graph = {0:[1], 1:[0,2], 2:[1,3,4], 3:[2], 4:[2,5], 5:[4]}
+		self.observed = [True,False,True,True,True,True]
 
-observed = [] #representing which calcium nodes have observations--k-hot vector
+		self.true_params = {'a':1, 'b':0, 'var_y':1, 'lambs':[1,2]}
 
-#will contain tuple of mean and var
-messages = {} #
+		self.lambs_ids = self.assign_lambdas([[0,1,2],[2,3,4,5]])
 
-a = 1 #emission variable
-b = 0 #emission variable
-var_y = 1 #variance sigma_y^2, emission variable
+		self.messages = {}
 
-lambs = {} #lambdas between the calcium nodes
+		self.calcium, self.data = self.gen_toy_data()
 
-data = [] #length of n, will include the observed y values for observed y
-#and None if unobserved  
+		self.root = 0
 
-def initialize():
-	for c_node in xrange(len(observed)):
-		if observed[c_node]: 
-			#this is the message from y_i to c_i
-			#tuple of mean and variance
-			messages[(_y(c_node),c_node)] = ((data[c_node] - b)/a,var_y/a**2)
-	for c_pair in calcium_pairs:
-		_make_lambda(c_pair[0],c_pair[1])
+		self.params = {'a':1, 'b':0, 'var_y':1, 'lambs':[1,2]}
 
-def send_message(source,destination,neighbors):
-'''neighbors is list of source's neighbors
-'''
-	#getting the variance of the message
-	var_m = 0
-	mu_m = 0
+		
 
-	for i in neighbors:
-		messages_to_source = messages[(i,source)]
-		var_m += 1./messages_to_source[1] #update var of message
-		mu_m += messages_to_source[0]/messages_to_source[1] #update mean of message
+		self.estimates = [None] * len(self.graph)
+		
 
-	if(observed[source]): #only if source is observed do we add the var_y
-		var_m += 1./var_y
-		mu_m += (data[source] - b) / (var_y * a) #data will only have source if it's observed
-	
-	var_m = 1./var_m
-	mu_m = mu_m * var_m
 
-	#setting the tuple
-	messages[(source,destination)] = (mu_m,var_m)
+	def assign_lambdas(self,groups):
+		lambs_ids = {}
+		for gi, g in enumerate(groups):
+			for node in g:
+				for neighbor in self.graph[node]:
+					if neighbor in g:
+						lambs_ids[(node,neighbor)] = gi
+		return lambs_ids
+
+	def gen_toy_data(self):
+		calcium = [None] * len(self.graph)
+		INIT_VAL = 10
+		calcium[0] = INIT_VAL
+
+		parents = {1:0, 2:1, 3:2, 4:2, 5:4}
+
+		for child in [1, 2, 3, 4, 5]:
+			#pdb.set_trace()
+			calcium[child] = calcium[parents[child]] + np.random.randn() * self.true_params['lambs'][self.lambs_ids[(child, parents[child])]]
+
+		data = [None] * len(self.graph)
+		for oi, obs in enumerate(self.observed):
+			if not obs:
+				data[oi] = None
+			else:
+				data[oi] = (self.true_params['a']*calcium[oi] + self.true_params['b']) + np.random.randn() * self.true_params['var_y']
+
+		return calcium, data
+
+
+	def send_message(self,source,destination):
+		#getting the variance of the message
+		var_m = 0
+		mu_m = 0
+
+		for k in self.graph[source]:
+			if k == destination:
+				pass
+			else:
+				messages_to_source = self.messages[(k,source)]
+				var_m += 1./messages_to_source[1] #update var of message
+				mu_m += messages_to_source[0]/messages_to_source[1] #update mean of message
+
+		if(self.observed[source]): #only if source is observed do we add the var_y
+			var_m += self.params['a']**2/self.params['var_y']
+			mu_m += (self.data[source] - self.params['b']) * self.params['a']/ (self.params['var_y']) #data will only have source if it's observed
+		
+		var_m = 1./var_m
+		mu_m = mu_m * var_m
+
+		#setting the tuple
+		self.messages[(source,destination)] = (mu_m,var_m)
+
+	def get_marginal(self,node):
+		var_m = 0
+		mu_m = 0
+
+		for k in self.graph[node]:
+			messages_to_node = self.messages[(k,node)]
+			var_m += 1./messages_to_node[1] #update var of message
+			mu_m += messages_to_node[0]/messages_to_node[1] #update mean of message
+
+		if(self.observed[node]): #only if source is observed do we add the var_y
+			var_m += self.params['a']**2/self.params['var_y']
+			mu_m += (self.data[node] - self.params['b']) * self.params['a']/ (self.params['var_y']) 
+
+		var_m = 1./var_m
+		mu_m = mu_m * var_m
+
+		return (mu_m, var_m)
+
+
+	def e_step(self):
+		self.message_passing()
+		estimates = [self.get_marginal(i) for i in xrange(len(self.graph))]
+		return estimates
+
+
+	def collect(self,i, j):
+		for k in self.graph[j]:
+			if k == i:
+				pass
+			else: 
+				self.collect(j, k)
+		self.send_message(j, i)
+
+	def distribute(self,i, j):
+		self.send_message(i, j)
+		for k in self.graph[j]:
+			if k == i:
+				pass
+			else:
+				self.distribute(j, k)
+
+	def message_passing(self):
+		for k in self.graph[self.root]:
+			self.collect(self.root,k)
+		for k in self.graph[self.root]:
+			self.distribute(self.root,k)
+
 
 #get index for obsereved variable
-def _y(c):
-	return c+n_nodes
+# def _y(c):
+# 	return c+n_nodes
 
-def _make_lambda(c_i,c_j):
-	#make the lambdas symmetric for both message directions
-	lambs[(c_i,c_j)] = 1
-	lambs[(c_j,c_i)] = 1
-
-n_calcium_edges = 5 
+# def _make_lambda(c_i,c_j):
+# 	#make the lambdas symmetric for both message directions
+# 	lambs[(c_i,c_j)] = 1
+# 	lambs[(c_j,c_i)] = 1
 
 if __name__ == '__main__':
-	initialize()
+	neuron = Neuron()
+	print neuron.e_step()
+	print neuron.calcium
 
-	#sending all the hard-coded messages 
-	# send_message()
+
+
+
+#a function to generate data
+#passes the observations to GBP() which will contain everything
+#GBP() will repeatedly call the E step (message passing) and M step
+#E step will spit out the marginals (estimates of Cs) and lambs
+#pass these marginals into M step
+#calculate ELBO
+#after loop is done, infer missing data 
+
+
