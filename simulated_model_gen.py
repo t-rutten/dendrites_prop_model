58e363ac71ccf197c73658fbf2c04b4ab33ba19b
Tom Rutten <t.rutten15@gmail.com>
2016-12-17 15:36:53 -0500

    random graphs

--- simulated_model_gen.py
+++ simulated_model_gen.py
@@ -10,7 +10,7 @@ class Node:
 		self.is_observation = is_observation
 
 
-def make_axon(graph, nums_to_nodes_map, num_nodes_bounds=[3,6]):
+def make_apical(graph, parents, num_nodes_bounds=[3,6]):
 	'''
 	generates nodes in axon
 	graph is list of adjacencies
@@ -18,25 +18,26 @@ def make_axon(graph, nums_to_nodes_map, num_nodes_bounds=[3,6]):
 	number of nodes is between min_num_nodes and max_num_nodes
 	'''
 	#get number of true calcium nodes in the axon according to bounds arg
-	num_axon_nodes = rnd.randint(num_nodes_bounds[0],num_nodes_bounds[1])
+	num_apical_nodes = rnd.randint(num_nodes_bounds[0],num_nodes_bounds[1])
 	
 	#make initial node, add to graph and nums_to_nodes_map
-	graph['0'] = []
-	nums_to_nodes_map['0'] = Node('0')
+	graph[0] = []
+	# nums_to_nodes_map['0'] = Node('0')
 
 	#add edges between other nodes
-	for i in xrange(1,num_axon_nodes):
+	for i in xrange(1,num_apical_nodes):
 		#add prev node to adjacency list of current node
-		graph[str(i)] = [str(i-1)]
-		
+		graph[i] = [i-1]
+		parents[i] = i-1
 		#add current node to adjacency list of prev node
-		graph[str(i-1)].append(str(i))
+		graph[i-1].append(i)
+
+	return num_apical_nodes-1
 		
 		#add current node to nums_to_nodes_map
-		nums_to_nodes_map[str(i)] = Node(str(i))
+		# nums_to_nodes_map[str(i)] = Node(str(i))
 
-def make_dendrites(graph, nums_to_nodes_map, num_branches_bounds=[2,4], 
-	branch_size_bounds=[1,3]):
+def make_dendritic(graph, parents, num_branches_bounds=[2,3], branch_size_bounds=[1,3]):
 	'''
 	generates dendritic branches
 	'''
@@ -54,50 +55,78 @@ def make_dendrites(graph, nums_to_nodes_map, num_branches_bounds=[2,4],
 		#get first node in branch (name is size of graph)
 		next_node_num = len(graph)
 
-		#connect soma to first node on branch and add first node on branch
-		graph[str(last_axon_node)].append(str(next_node_num))
-		graph[str(next_node_num)] = []
-		graph[str(next_node_num)].append(str(last_axon_node))
-		nums_to_nodes_map[str(next_node_num)] = Node(str(next_node_num))
+		#connect cell body to first node on branch and add first node on branch
+		graph[last_axon_node].append(next_node_num)
+		graph[next_node_num] = []
+		graph[next_node_num].append(last_axon_node)
+		parents[next_node_num] = last_axon_node
+		# nums_to_nodes_map[str(next_node_num)] = Node(str(next_node_num))
 
 		#add nodes to branches, also add them to nums_to_nodes_map
 		for i in xrange(len(graph),len(graph)+branch_size):
-			if str(i) not in graph.keys():
-				graph[str(i)] = [str(i-1)]
-				graph[str(i-1)].append(str(i))
-				nums_to_nodes_map[str(i)] = Node(str(i))
+			if i not in graph.keys():
+				graph[i] = [i-1]
+				parents[i] = i-1
+				graph[i-1].append(i)
 
-def make_observations(graph, nums_to_nodes_map,
-	ratio_of_evidence_nodes=0.5):
-	'''
-	adds evidence nodes
-	number of evidence nodes is according to ratio arg
-	'''
-	#determine which nodes will have observed calcium
-	calcium_nodes_w_observations = rnd.choice(range(len(graph)),
-		int(ratio_of_evidence_nodes * len(graph)),False)
+				# nums_to_nodes_map[str(i)] = Node(str(i))
 
-	#connect evidence nodes to corresponding true calcium nodes
-	#add evidence nodes to graph nodes and nums_to_nodes_map
-	for c in calcium_nodes_w_observations:
-		current_node_num = len(graph)
-		graph[str(c)].append(str(current_node_num))
-		graph[str(current_node_num)] = [str(c)]
-		nums_to_nodes_map[str(current_node_num)] = Node(str(current_node_num),True)
+# def make_observations(graph, nums_to_nodes_map,
+# 	ratio_of_evidence_nodes=0.5):
+# 	'''
+# 	adds evidence nodes
+# 	number of evidence nodes is according to ratio arg
+# 	'''
+# 	#determine which nodes will have observed calcium
+# 	calcium_nodes_w_observations = rnd.choice(range(len(graph)),
+# 		int(ratio_of_evidence_nodes * len(graph)),False)
+
+# 	#connect evidence nodes to corresponding true calcium nodes
+# 	#add evidence nodes to graph nodes and nums_to_nodes_map
+# 	for c in calcium_nodes_w_observations:
+# 		current_node_num = len(graph)
+# 		graph[str(c)].append(str(current_node_num))
+# 		graph[str(current_node_num)] = [str(c)]
+# 		nums_to_nodes_map[str(current_node_num)] = Node(str(current_node_num),True)
+
+def make_two_lamb_groups(soma,graph):
+	return [range(0,soma+1),range(soma,len(graph))]
+
+
+def make_graph():
+	graph = {}
+	parents = {}
+
+	soma_node = make_apical(graph,parents)
+	make_dendritic(graph,parents)
+	lamb_groups = make_two_lamb_groups(soma_node,graph)
+
+
+	return graph, parents, lamb_groups
+
+
 
 	
 if __name__=='__main__':
 	#this example creates one random graph; we can easily create many graphs
-	graph = {}
-	nums_to_nodes = {}
+	# graph = {}
+	# nums_to_nodes = {}
 
-	make_axon(graph,nums_to_nodes)
-	make_dendrites(graph,nums_to_nodes)
-	make_observations(graph,nums_to_nodes)
+	# make_apical(graph,nums_to_nodes)
+	# make_dendritic(graph,nums_to_nodes)
+	# make_observations(graph,nums_to_nodes)
+
+	graph, parents, lamb_group_ids = make_graph()
 
 
 	for k in graph:
 		print 'node: ' + str(k) + ', connected to: ' + str(graph[k])
-		print '\t' + 'node name in nums_to_nodes map (should match above): ' + nums_to_nodes[k].name
-		print '\t' + 'is evidence node: ' + str(nums_to_nodes[k].is_observation)
-		print ''
+		# print '\t' + 'node name in nums_to_nodes map (should match above): ' + nums_to_nodes[k].name
+		# print '\t' + 'is evidence node: ' + str(nums_to_nodes[k].is_observation)
+	print '\n'
+	print 'parents:'
+	for p in parents:
+		print "%d, %d" % (p,parents[p])
+	print '\n'
+	print "lamb groups: %s\n" % str(lamb_group_ids)
+
